670b0a4b5855f7cd746d4324e8b4d47dc30a4885
Chang <115979788+cih1235@users.noreply.github.com>
2025-01-07 09:29:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from PIL import Image
 from Read import initialize_model, Read_Data
-from Search import Search_by_Image, Search_by_Word, Search_by_ImageAndText
 # 初始化模型和數據
 @st.cache_resource
 def initialize_data():
@@ -31,18 +30,7 @@ def main():
         if uploaded_image:
             image = Image.open(uploaded_image)
             st.image(image, caption="上傳的圖片", use_container_width=True)
-            if st.button("開始搜尋"):
-                results = Search_by_Image(recipes, image, model, processor, top_k=5)
-                st.write("搜尋結果：")
-                for result in results:
-                    st.subheader(f"品名：{result['品名']}")
-                    if result["圖片路徑"]:
-                        st.image(result["圖片路徑"], caption=f"相似度：{result['相似度']:.4f}", use_container_width=True)
-                    else:
-                        st.warning(f"{result['品名']} 的圖片未找到")
-                    st.write(f"食材：{result['食材']}")
-                    st.write(f"步驟：{result['步驟']}")
-                    st.write("-" * 50)
+            
                     
     # 功能 2：透過文字搜尋食譜
     elif option == "透過文字搜尋食譜":
@@ -56,20 +44,7 @@ def main():
             openai_api_key = st.text_input('key:', type='password')
 
         search_query = st.text_input("請輸入您的描述，例如：清淡的蔬菜料理")
-        if st.button("開始搜尋"):
-            if search_query:
-                st.write(f"搜尋描述：{search_query}")
-                results = Search_by_Word(recipes, clip_or_langchain, openai_api_key, search_query, model, processor, tokenizer, top_k=5)
-
-                # 顯示搜尋結果
-                st.write("搜尋結果：")
-                for result in results:
-                    st.subheader(f"品名：{result['品名']}")
-                    st.image(result["圖片路徑"], caption=f"相似度：{result['相似度']:.4f}", use_container_width=True)
-                    st.write(f"步驟：{result['步驟']}")
-                    st.write("-" * 50)
-            else:
-                st.warning("請輸入描述後再點擊搜尋")
+        
 
 
     # 功能 3：透過圖片與文字搜尋食譜
@@ -82,20 +57,6 @@ def main():
         if uploaded_image and search_query:
             image = Image.open(uploaded_image)
             st.image(image, caption="上傳的圖片", use_container_width=True)
-            if st.button("開始搜尋"):
-                st.write(f"搜尋描述：{search_query}")
-                results = Search_by_ImageAndText(recipes, image, search_query, model, processor, tokenizer, top_k=5)
-                
-                st.write("搜尋結果：")
-                for result in results:
-                    st.subheader(f"品名：{result['品名']}")
-                    if result["圖片路徑"]:
-                        st.image(result["圖片路徑"], caption=f"相似度：{result['相似度']:.4f}", use_container_width=True)
-                    else:
-                        st.warning(f"{result['品名']} 的圖片未找到")
-                    st.write(f"食材：{result['食材']}")
-                    st.write(f"步驟：{result['步驟']}")
-                    st.write("-" * 50)
         elif not uploaded_image:
             st.warning("請上傳圖片後再進行搜尋")
         elif not search_query:
